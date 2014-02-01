dae756512c6c0447b0042ab33b30b638f9acf7b2
Vidal Graupera <vgraupera@gmail.com>
2014-02-01 06:46:55 -0800

    add configuration

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,20 @@
+var note = localStorage.getItem('note') || '';
+
+Pebble.addEventListener('showConfiguration', function(e) {
+	var uri = 'https://rawgithub.com/Neal/pebble-package-trackr/master/html/configuration.html?' +
+				'note=' + encodeURIComponent(note);
+	console.log('showing configuration at uri: ' + uri);
+	Pebble.openURL(uri);
+});
+
+Pebble.addEventListener('webviewclosed', function(e) {
+	console.log('configuration closed');
+	if (e.response) {
+		var options = JSON.parse(decodeURIComponent(e.response));
+		console.log('options received from configuration: ' + JSON.stringify(options));
+		apiKey = options['note'];
+		localStorage.setItem('note', apiKey);
+	} else {
+		console.log('no options received');
+	}
+});
